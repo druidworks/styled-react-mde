e78fc809275802e22df0f658fe1b1cf6ac218543
Kelly Truter <kellytruter@gmail.com>
2019-01-08 22:46:42 +0200

    WIP: Add prefix line methods

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -45,7 +45,7 @@ export class Editor extends React.Component<EditorProps, EditorState> {
       <div>
         {!this.props.headless && <div>header</div>}
         <textarea
-          ref={c => {
+          ref={(c) => {
             this.textarea = c;
           }}
           placeholder={this.props.placeholder}
@@ -64,7 +64,7 @@ export class Editor extends React.Component<EditorProps, EditorState> {
 
   public componentDidMount() {
     const addEventListeners = () =>
-      this.selectionUpdateEvents.forEach(eventType => {
+      this.selectionUpdateEvents.forEach((eventType) => {
         if (this.textarea !== null) {
           this.textarea.addEventListener(
             eventType,
@@ -77,7 +77,7 @@ export class Editor extends React.Component<EditorProps, EditorState> {
 
   public componentWillUnmount() {
     const removeEventListeners = () =>
-      this.selectionUpdateEvents.forEach(eventType => {
+      this.selectionUpdateEvents.forEach((eventType) => {
         if (this.textarea !== null) {
           this.textarea.removeEventListener(
             eventType,
@@ -86,6 +86,19 @@ export class Editor extends React.Component<EditorProps, EditorState> {
         }
       });
     removeEventListeners();
+  }
+
+  private updateState(newState: any, callback?: () => void) {
+    if (newState && newState.content) {
+      this.setState(
+        Object.assign({}, newState, {
+          content: newState.content.replace(/ +/g, " ")
+        }),
+        callback
+      );
+    } else {
+      this.setState(newState, callback);
+    }
   }
 
   private onChange() {
@@ -106,7 +119,7 @@ export class Editor extends React.Component<EditorProps, EditorState> {
       this.props.updateSelectionHandler(selection);
     }
 
-    this.setState(
+    this.updateState(
       {
         content: updatedContent,
         selection: updatedSelection
@@ -124,7 +137,7 @@ export class Editor extends React.Component<EditorProps, EditorState> {
     if (this.props.updateSelectionHandler) {
       this.props.updateSelectionHandler(selection);
     } else {
-      this.setState({
+      this.updateState({
         selection
       });
     }
@@ -151,14 +164,21 @@ export class Editor extends React.Component<EditorProps, EditorState> {
         case Key.B:
           this.wrapSelection("**", /\*\*/);
           break;
-        case Key.U:
-          this.wrapSelection("_", /\_/);
+        case Key.T:
+          this.wrapSelection("`", /\`/);
           break;
         case Key.I:
           this.wrapSelection("_", /\_/);
           break;
+        case Key.Q:
+          this.prefixLine(">");
+          break;
+        case Key.P:
+          this.prefixLine("*");
+          break;
       }
     }
+    // monitor tab / shift keys are held
   }
 
   private getSelectionOffset(offset: number = 0) {
@@ -172,19 +192,34 @@ export class Editor extends React.Component<EditorProps, EditorState> {
     };
   }
 
+  private sliceContent(start: number, end: number) {
+    return this.state.content.slice(start, end);
+  }
+
   private getSelectedText(offset: number = 0) {
     const selectionOffset = this.getSelectionOffset(offset);
-    return this.state.content.slice(selectionOffset.start, selectionOffset.end);
+    return this.sliceContent(selectionOffset.start, selectionOffset.end);
+  }
+
+  private replaceSubString(
+    content: string,
+    text: string,
+    start: number,
+    end: number
+  ) {
+    return `${content.substr(0, start)}${text}${content.substr(end)}`;
   }
 
   private updatedSelectedText(text: string, offset: number = 0) {
     const selectionOffset = this.getSelectionOffset(offset);
     let content = this.state.content;
-    content =
-      content.substr(0, selectionOffset.start) +
-      text +
-      content.substr(selectionOffset.end);
-    this.setState({
+    content = this.replaceSubString(
+      content,
+      text,
+      selectionOffset.start,
+      selectionOffset.end
+    );
+    this.updateState({
       content,
       selection: {
         startOffset: selectionOffset.start,
@@ -216,5 +251,67 @@ export class Editor extends React.Component<EditorProps, EditorState> {
     } else if (exactText !== "") {
       this.updatedSelectedText(`${tag}${exactText}${tag}`); // TODO: cater for white space on ends and move tags flush with word ends
     }
+  }
+
+  private prefixLine(tag: string) {
+    let content = this.state.content;
+    const lines = content.split(/\r|\r\n|\n/);
+    const selection = this.state.selection;
+
+    let currentLength = 0;
+    let selectionStartFound = false;
+    for (let index = 0; index < lines.length; index++) {
+      const line = lines[index];
+      const lineStart = currentLength;
+      const lineEnd = currentLength + line.length;
+      const selectionStartInLine =
+        selection.startOffset >= lineStart && selection.startOffset <= lineEnd;
+      const selectionEndInLine =
+        selection.endOffset >= lineStart && selection.endOffset <= lineEnd;
+      const lineContent = this.sliceContent(lineStart, lineEnd);
+      const lineContentStartsWithTag = lineContent.startsWith(`${tag} `);
+
+      let canPrefix = false;
+      let canBreak = false;
+      if (selectionStartInLine && selectionEndInLine) {
+        canPrefix = true;
+        canBreak = true;
+      } else if (selectionStartInLine) {
+        selectionStartFound = true;
+        canPrefix = true;
+      } else if (selectionEndInLine) {
+        selectionStartFound = true;
+        canPrefix = true;
+        canBreak = true;
+      } else if (selectionStartFound) {
+        canPrefix = true;
+      }
+
+      if (canPrefix) {
+        // TODO: find index & length of each split value per line and calculate true line start & end for replacement.
+        if (lineContentStartsWithTag) {
+          content = this.replaceSubString(
+            content,
+            lineContent.replace(`${tag} `, ""),
+            lineStart > 0 ? lineStart + 1 : 0,
+            lineEnd
+          );
+        } else {
+          content = this.replaceSubString(
+            content,
+            `${tag} ${lineContent}`,
+            lineStart > 0 ? lineStart + 1 : 0,
+            lineEnd
+          );
+        }
+      }
+
+      if (canBreak) {
+        break;
+      }
+
+      currentLength += line.length;
+    }
+    this.updateState({ content, selection });
   }
 }
